93ffb7c106a707f4011ed90f49ed4f08efb0f6e9
songyipan <3515164447@qq.com>
2026-02-12 18:26:26 +0800

    chore: 删除dist目录下的生成文件

--- dist/flow/new-project.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const downloadProject: (name: string, apiKey: string) => Promise<void>;
-export declare const createNewProject: () => Promise<void>;
-//# sourceMappingURL=new-project.d.ts.map
--- dist/flow/push-work-flow.d.ts
+++
@@ -1,4 +0,0 @@
-import { PushSkillParams } from '../../types/index.js';
-export declare const runPushWorkflow: () => Promise<void>;
-export declare const handlePushSkill: ({ repoUrl, skillName }: PushSkillParams) => Promise<void>;
-//# sourceMappingURL=push-work-flow.d.ts.map
